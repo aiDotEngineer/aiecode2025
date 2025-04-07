55b40e3a1f43c0b1dc6df239eb0d56c4b5e3b510
swyxio <shawnthe1@gmail.com>
2025-04-07 12:23:54 -0700

    usebeforefiles rewrites

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,11 @@
 import type { NextConfig } from "next";
 
 const mappings = {
-  'https://aie-summit.vercel.app/summit/2025': [
-    '/summit',
-    '/summit/2025',
+  'https://aie-summit.vercel.app/summit/:path*': [
+    '/summit/:path*'
   ],
-  'https://aie-summit.vercel.app/summit/2023': [
-    '/summit/2023',
-  ],
-  'https://aie-summit.vercel.app/worldsfair': [
-    '/worldsfair/2024',
-  ],
-  'https://aie-summit.vercel.app/worldsfair/2024': [
-    '/worldsfair/2024',
+  'https://aie-summit.vercel.app/worldsfair/:path*': [
+    '/worldsfair/:path*',
   ],
   'https://x.com/@aidotengineer': [
     '/twitter',
@@ -25,19 +18,24 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   // https://nextjs.org/docs/app/api-reference/config/next-config-js/rewrites
   async rewrites() {
-    return [
-      ...Object.entries(mappings).flatMap(([destination, sources]) =>
-        sources.map(source => ({
-          source,
-          destination,
-        }))
-      ),
-      // Add any manual rewrites below
-      {
-        source: '/youtube',
-        destination: 'https://youtube.com/@aidotengineer',
-      }
-    ];
+    return {
+
+      beforeFiles: [
+        ...Object.entries(mappings).flatMap(([destination, sources]) =>
+          sources.map(source => ({
+            source,
+            destination,
+          }))
+        ),
+      ],
+      fallback: [
+        // Add any manual rewrites below
+        {
+          source: '/youtube',
+          destination: 'https://youtube.com/@aidotengineer',
+        }
+      ]
+    }
   },
 };
 
