51f22e32fbd54431dde6e5bcaf449c2e4d975a25
swyxio <shawnthe1@gmail.com>
2025-04-07 12:19:47 -0700

    add rewrites

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,44 @@
 import type { NextConfig } from "next";
 
+const mappings = {
+  'https://aie-summit.vercel.app/summit/2025': [
+    '/summit',
+    '/summit/2025',
+  ],
+  'https://aie-summit.vercel.app/summit/2023': [
+    '/summit/2023',
+  ],
+  'https://aie-summit.vercel.app/worldsfair': [
+    '/worldsfair/2024',
+  ],
+  'https://aie-summit.vercel.app/worldsfair/2024': [
+    '/worldsfair/2024',
+  ],
+  'https://x.com/@aidotengineer': [
+    '/twitter',
+    '/x'
+  ],
+}
+
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  // https://nextjs.org/docs/app/api-reference/config/next-config-js/rewrites
+  async rewrites() {
+    return [
+      ...Object.entries(mappings).flatMap(([destination, sources]) =>
+        sources.map(source => ({
+          source,
+          destination,
+        }))
+      ),
+      // Add any manual rewrites below
+      {
+        source: '/youtube',
+        destination: 'https://youtube.com/@aidotengineer',
+      }
+    ];
+  },
 };
 
 export default nextConfig;
