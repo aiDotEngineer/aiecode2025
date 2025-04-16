b57e475ced74dccad2b68f5658bd83dfd827d1fd
Brad Westfall <brad@builtwithweb.com>
2025-04-16 16:13:19 -0400

    fixes #2

--- src/components/worldsfair-2025/Sponsors.tsx
+++ src/components/worldsfair-2025/Sponsors.tsx
@@ -1,4 +1,4 @@
-import { Logo } from '../Logos';
+import { Logo } from "../Logos";
 
 export function Sponsors() {
   return (
@@ -17,29 +17,394 @@ export function Sponsors() {
         </p>
       </header>
 
+      <div className="uppercase font-bold text-gray-on-white max-md:text-center">
+        Presenting Sponsor
+      </div>
+      <div className="flex gap-12 max-md:flex-col">
+        <div className="md:flex-1 max-md:text-center">
+          <a
+            href="https://azure.microsoft.com/en-us/solutions/ai"
+            target="_blank"
+            className="inline-block"
+          >
+            <Logo
+              logo="microsoft"
+              variant="colorGray"
+              className="inline-block h-20 w-72 md:h-32 md:w-full object-contain"
+            />
+          </a>
+        </div>
+        <div className="md:flex-1 text-gray-on-white">
+          In this new era of AI, Microsoft is helping organizations unlock AI
+          innovation in every business, in every app, for everyone. Microsoft
+          Azure delivers AI services and supercomputing infrastructure
+          purpose-built to meet the compute-intensive needs of AI and
+          high-performance workloads. Our full stack AI solution meets the most
+          stringent security, reliability and compliance requirements while
+          providing exceptional performance, flexibility and cost efficiencies.
+          Choose Microsoft Azure for your groundbreaking AI applications.
+        </div>
+      </div>
+
+      <div className="uppercase font-bold text-gray-on-white max-md:text-center">
+        Innovation Partner
+      </div>
+      <div className="flex gap-12 max-md:flex-col">
+        <div className="md:flex-1 max-md:text-center">
+          <a
+            href="https://aws.amazon.com/ai"
+            target="_blank"
+            className="inline-block"
+          >
+            <Logo
+              logo="aws"
+              className="inline-block h-40 w-60 md:h-44 md:w-full object-contain"
+            />
+          </a>
+        </div>
+        <div className="md:flex-1 text-gray-on-white">
+          Leverage over 25 years of Amazon's pioneering AI experience to scale
+          your innovations. AWS makes AI accessible to AI Engineers through
+          services like Amazon Bedrock which offers capabilities like RAG,
+          agents, guardrails, and access to leading foundation models including
+          Amazon's own state-of-the-art (SOTA) Nova models, as well as Amazon Q,
+          the most capable generative AI-powered assistant to help transform how
+          work gets done. AWS empowers its customers to meet business demands,
+          embrace generative AI, and build on a foundation of privacy,
+          end-to-end security, and AI governance.
+        </div>
+      </div>
+
+      <div className="space-y-0 text-center">
+        <div className="uppercase font-bold text-gray-on-white">
+          Platinum Sponsors
+        </div>
+
+        <a href="https://windsurf.com" target="_blank" className="inline-block">
+          <Logo
+            logo="windsurf"
+            className="inline-block h-20 w-48 md:h-32 md:w-64 object-contain mx-6"
+          />
+        </a>
+        <a href="https://mongodb.com" target="_blank">
+          <Logo
+            logo="mongodb"
+            className="inline-block h-20 w-48 md:h-32 md:w-64 object-contain mx-6"
+          />
+        </a>
+        <a href="https://graphite.dev" target="_blank" className="inline-block">
+          <Logo
+            logo="graphite"
+            variant="black"
+            className="inline-block h-20 w-48 md:h-32 md:w-64 object-contain mx-6"
+          />
+        </a>
+        <a href="https://daily.co" target="_blank">
+          <Logo
+            logo="daily"
+            variant="black"
+            className="inline-block h-10 w-48 md:h-28 md:w-42 object-contain mx-6"
+          />
+        </a>
+      </div>
+
+      <div className="space-y-6 text-center">
+        <div className="uppercase font-bold text-gray-on-white">
+          Gold Sponsors
+        </div>
+        <a href="https://neo4j.com" target="_blank" className="inline-block">
+          <Logo
+            logo="neo4j"
+            className="inline-block h-18 w-32 md:h-18 md:w-42 object-contain mx-6"
+          />
+        </a>
+        <a href="https://hasura.io" target="_blank" className="inline-block">
+          <Logo
+            logo="hasura"
+            className="inline-block h-18 w-32 md:h-24 md:w-48 object-contain mx-6"
+          />
+        </a>
+        <a
+          href="https://rungalileo.io"
+          target="_blank"
+          className="inline-block"
+        >
+          <Logo
+            logo="galileo"
+            variant="colorBlack"
+            className="inline-block h-18 w-32 md:h-24 md:w-48 object-contain mx-6"
+          />
+        </a>
+        <a
+          href="https://sourcegraph.com"
+          target="_blank"
+          className="inline-block"
+        >
+          <Logo
+            logo="sourcegraph"
+            className="inline-block h-18 w-44 md:h-24 md:w-64 object-contain mx-6"
+          />
+        </a>
+        <a
+          href="https://llamaindex.ai"
+          target="_blank"
+          className="inline-block"
+        >
+          <Logo
+            logo="llamaIndex"
+            variant="black"
+            className="inline-block h-18 w-32 md:h-24 md:w-64 object-contain mx-6"
+          />
+        </a>
+        <a href="https://temporal.io" target="_blank" className="inline-block">
+          <Logo
+            logo="temporal"
+            variant="black"
+            className="inline-block h-18 w-32 md:h-24 md:w-52 object-contain mx-6"
+          />
+        </a>
+        <a href="https://baseten.co/" target="_blank" className="inline-block">
+          <Logo
+            logo="baseten"
+            className="inline-block h-18 w-32 md:h-24 md:w-48 object-contain mx-6"
+          />
+        </a>
+        <a href="https://elastic.co" target="_blank" className="inline-block">
+          <Logo
+            logo="elastic"
+            className="inline-block h-18 w-32 md:h-24 md:w-48 object-contain mx-6"
+          />
+        </a>
+        <a
+          href="https://withorb.com/ai"
+          target="_blank"
+          className="inline-block"
+        >
+          <Logo
+            logo="orb"
+            variant="black"
+            className="inline-block h-18 w-32 md:h-24 md:w-48 object-contain mx-6"
+          />
+        </a>
+        <a href="https://gitpod.io" target="_blank" className="inline-block">
+          <Logo
+            logo="gitpod"
+            className="inline-block h-18 w-32 md:h-24 md:w-48 object-contain mx-6"
+          />
+        </a>
+        <a href="https://freeplay.ai" target="_blank" className="inline-block">
+          <Logo
+            logo="freeplay"
+            variant="black"
+            className="inline-block h-18 w-44 md:h-24 md:w-64 object-contain mx-6"
+          />
+        </a>
+        <a href="https://dagger.io/" target="_blank" className="inline-block">
+          <Logo
+            logo="dagger"
+            className="inline-block h-18 w-32 md:h-24 md:w-48 object-contain mx-6"
+          />
+        </a>
+        <a
+          href="https://traceloop.com"
+          target="_blank"
+          className="inline-block"
+        >
+          <Logo
+            logo="traceloop"
+            variant="color"
+            className="inline-block h-18 w-44 md:h-24 md:w-64 object-contain mx-6"
+          />
+        </a>
+        <a
+          href="https://ai.pydantic.dev"
+          target="_blank"
+          className="inline-block"
+        >
+          <Logo
+            logo="pydantic"
+            variant="color"
+            className="inline-block h-18 w-44 md:h-24 md:w-64 object-contain mx-6"
+          />
+        </a>
+      </div>
+
+      <div className="space-y-6 text-center">
+        <div className="uppercase font-bold text-gray-on-white">
+          Silver Sponsors
+        </div>
+        <a href="https://agentuity.ai" target="_blank" className="inline-block">
+          <Logo
+            logo="agentuity"
+            className="inline-block h-12 w-42 md:h-20 md:w-44 object-contain mx-6"
+          />
+        </a>
+        <a
+          href="https://weightsandbiases.com"
+          target="_blank"
+          className="inline-block"
+        >
+          <Logo
+            logo="weightsBiases"
+            className="inline-block h-12 w-42 md:h-20 md:w-64 object-contain mx-6"
+          />
+        </a>
+        <a href="https://sierra.ai" target="_blank" className="inline-block">
+          <Logo
+            logo="sierra"
+            className="inline-block h-12 w-30 md:h-20 md:w-42 object-contain mx-6"
+          />
+        </a>
+        <a href="https://vellum.com" target="_blank" className="inline-block">
+          <Logo
+            logo="vellum"
+            className="inline-block h-12 w-24 md:h-20 md:w-42 object-contain mx-6"
+          />
+        </a>
+        <a href="https://gensx.com" target="_blank" className="inline-block">
+          <Logo
+            logo="gensx"
+            variant="black"
+            className="inline-block h-12 w-32 md:h-20 md:w-44 object-contain mx-6"
+          />
+        </a>
+        <a href="https://redis.com" target="_blank" className="inline-block">
+          <Logo
+            logo="redis"
+            className="inline-block h-12 w-32 md:h-20 md:w-36 object-contain mx-6"
+          />
+        </a>
+        <a href="https://langbase.ai" target="_blank" className="inline-block">
+          <Logo
+            logo="langbase"
+            variant="black"
+            className="inline-block h-12 w-32 md:h-20 md:w-36 object-contain mx-6"
+          />
+        </a>
+        <a href="https://twilio.com" target="_blank" className="inline-block">
+          <Logo
+            logo="twilio"
+            className="inline-block h-12 w-32 md:h-20 md:w-36 object-contain mx-6"
+          />
+        </a>
+        <a href="https://descope.com" target="_blank" className="inline-block">
+          <Logo
+            logo="descope"
+            className="inline-block h-12 w-32 md:h-20 md:w-44 object-contain mx-6"
+          />
+        </a>
+        <a
+          href="https://superannotate.com"
+          target="_blank"
+          className="inline-block"
+        >
+          <Logo
+            logo="superannotate"
+            className="inline-block h-12 w-48 md:h-20 md:w-64 object-contain mx-6"
+          />
+        </a>
+        <a
+          href="https://unstructured.io"
+          target="_blank"
+          className="inline-block"
+        >
+          <Logo
+            logo="unstructured"
+            className="inline-block h-12 w-12 md:h-20 md:w-20 object-contain mx-6"
+          />
+        </a>
+        <a href="https://baz.co" target="_blank" className="inline-block">
+          <Logo
+            logo="baz"
+            variant="black"
+            className="inline-block h-12 w-18 md:h-20 md:w-28 object-contain mx-6"
+          />
+        </a>
+        <a href="https://vessl.ai" target="_blank" className="inline-block">
+          <Logo
+            logo="vessl"
+            variant="black"
+            className="inline-block h-12 w-32 md:h-20 md:w-54 object-contain mx-6"
+          />
+        </a>
+        <a href="https://riza.ai" target="_blank" className="inline-block">
+          <Logo
+            logo="riza"
+            variant="black"
+            className="inline-block h-12 w-16 md:h-20 md:w-24 object-contain mx-6"
+          />
+        </a>
+        <a href="https://tambo.co" target="_blank" className="inline-block">
+          <Logo
+            logo="tambo"
+            className="inline-block h-12 w-42 md:h-20 md:w-44 object-contain mx-6"
+          />
+        </a>
+        <a href="https://sentry.io" target="_blank" className="inline-block">
+          <Logo
+            logo="sentry"
+            variant="black"
+            className="inline-block h-12 w-32 md:h-20 md:w-44 object-contain mx-6"
+          />
+        </a>
+        <a href="https://xpander.ai" target="_blank" className="inline-block">
+          <Logo
+            logo="xpander"
+            variant="black"
+            className="inline-block h-12 w-32 md:h-20 md:w-54 object-contain mx-6"
+          />
+        </a>
+        <a
+          href="https://thomsonreuters.com"
+          target="_blank"
+          className="inline-block"
+        >
+          <Logo
+            logo="thompsonReuters"
+            className="inline-block h-12 w-48 md:h-20 md:w-64 object-contain mx-6"
+          />
+        </a>
+      </div>
+      <div className="space-y-6 text-center">
+        <div className="uppercase font-bold text-gray-on-white">Supporters</div>
+        <a href="https://circle.com" target="_blank" className="inline-block">
+          <Logo
+            logo="circle"
+            className="inline-block h-12 w-42 md:h-20 md:w-44 object-contain mx-6"
+          />
+        </a>
+      </div>
+
       <div className="text-center space-y-6 p-8 bg-stone-200 rounded-2xl">
         <p className="text-xl">
-          <span className="uppercase font-mono bg-red-600 p-1 rounded-sm text-yellow-100">CLOSING SOON</span>
+          <span className="uppercase font-mono bg-red-600 p-1 rounded-sm text-yellow-100">
+            CLOSING SOON
+          </span>
         </p>
         <p className="text-gray-700">
-          To learn more about sponsorship opportunities for World's Fair 2025, please email{' '}
-          <a href="mailto:sponsors@ai.engineer" className="text-blue-500 hover:text-blue-400">
-            sponsors@ai.engineer
+          To learn more about sponsorship opportunities for World's Fair 2025,
+          please email{" "}
+          <a
+            href="mailto:sponsorships@ai.engineer"
+            className="text-blue-500 hover:text-blue-400"
+          >
+            sponsorships@ai.engineer
           </a>
         </p>
         <p className="text-gray-600">
-          You can view our previous supporters at:{' '}
-          <a href="/worldsfair/2024" className="text-blue-500 hover:text-blue-400">
+          You can view our previous supporters at:{" "}
+          <a
+            href="/worldsfair/2024"
+            className="text-blue-500 hover:text-blue-400"
+          >
             World's Fair 2024
-          </a>
-          {' '}and{' '}
+          </a>{" "}
+          and{" "}
           <a href="/summit/2025" className="text-blue-500 hover:text-blue-400">
             Summit 2025
           </a>
         </p>
       </div>
-
     </div>
   );
 }
-
